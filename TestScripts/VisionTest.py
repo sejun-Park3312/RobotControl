8c096b69cf126cca700dec9d39c0dfef062687a3
Acer <dlrhddlfvkf21207@pusan.ac.kr>
2025-07-30 01:58:24 +0900

    Fix merge conflicts

--- TestScripts/VisionTest.py
+++ TestScripts/VisionTest.py
@@ -3,5 +3,8 @@ import time
 from ProjectPath import PROJECT_PATH
 from ClassFiles.Vision import Vision
 
+# 먼저 연결된 웹캠의 번호 확인 후 Vision에 주기
+# v4l2-ctl --list-devices
+# sudo apt install v4l-utils (위에 설치 필요 시)
 VS = Vision()
 VS.Tracking()
